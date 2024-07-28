b3cdc9820ea67b5659d7f5c3089156ca8bbf9921
salihalammari <slammari@student.1337.ma>
2024-07-27 18:05:57 -0700

    please work

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-controllerRouting(app);
+
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
-export default app;
+app.use('/', indexRouter);
+
+module.export  app;
