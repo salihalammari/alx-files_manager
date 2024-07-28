9adf36064cacd59214df31574f69f45862bd057a
salihalammari <slammari@student.1337.ma>
2024-07-28 13:50:12 -0700

    update all config

--- server.js
+++ server.js
@@ -1,28 +1,15 @@
 import express from 'express';
-import router from './routes/index';
-import unmatchedRouteHandler from './middleware/unmatched';
-import errorHandler from './middleware/error';
-import shutdown from './utils/shutdown';
+import indexRouter from './routes/index';
 
-// Express server
 const app = express();
-const PORT = process.env.PORT || 5000;
+
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(router);
-app.use(unmatchedRouteHandler);
-app.use(errorHandler);
 
-const server = app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Server running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
+app.use('/', indexRouter);
 
-// Graceful shutdown
-const handler = () => shutdown(server);
-process.on('SIGINT', handler);
-process.on('SIGTERM', handler);
-process.on('SIGQUIT', handler);
-
-export default app;
+module.exports = app;
