dc3732c8d3bf06450df1475b0b2df30d3b8b1005
salihalammari <slammari@student.1337.ma>
2024-07-27 19:19:32 -0700

    add all

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
 import express from 'express';
-import indexRouter from './routes/index';
+import router from './routes/index';
+import unmatchedRouteHandler from './middleware/unmatched';
+import errorHandler from './middleware/error';
+import shutdown from './utils/shutdown';
 
+// Express server
 const app = express();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(router);
+app.use(unmatchedRouteHandler);
+app.use(errorHandler);
 
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+const server = app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server running on port ${PORT}`);
 });
 
-app.use('/', indexRouter);
+// Graceful shutdown
+const handler = () => shutdown(server);
+process.on('SIGINT', handler);
+process.on('SIGTERM', handler);
+process.on('SIGQUIT', handler);
 
-module.export  app;
+export default app;
