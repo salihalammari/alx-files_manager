cff703393b5b05a125149be0c1612622f4a285d8
salihalammari <slammari@student.1337.ma>
2024-07-31 07:02:33 -0700

    last modif

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 import express from 'express';
-import indexRouter from './routes/index';
+import controllerRouting from './routes/index';
 
 const app = express();
-
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+controllerRouting(app);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-app.use('/', indexRouter);
 
-module.exports = app;
+export default app;
